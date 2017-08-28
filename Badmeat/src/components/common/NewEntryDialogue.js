36244c635ee60f69d46e40ae341b1f780d55e06f
deonj197 <deonj197@gmail.com>
2017-08-28 00:03:55 -0400

    Moved title into header bar. Formatted its styling.
    Added a title prop to the HeaderButton popup screens. The title is displayed withing the blue header bar, to the right of the back button, with adequate spacing between them.

--- Badmeat/src/components/common/NewEntryDialogue.js
+++ Badmeat/src/components/common/NewEntryDialogue.js
@@ -9,16 +9,14 @@ const styles = StyleSheet.create({
   container:{
     marginRight: 10,
   },
-  title:{
-    fontSize: 24
-  }
+
 });
 
 class NewEntryDialogue extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Add New Entry</Text>
+        <Text >Enter details or use the camera!</Text>
       </View>
     );
   }
